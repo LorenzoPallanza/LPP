99a6b85c929611b2e961f7a8c00c64d84097d129
Lorenzo Pallanza <lorenzo.pallanza.98@gmail.com>
2021-11-12 17:35:41 +0100

    added fibonacci.hs in penny.hs

--- penny.hs
+++ penny.hs
@@ -4,3 +4,30 @@ assoluto n = if n >= 0 then n else negate n
 
 anno_terra :: Double
 anno_terra = 2 * pi * 150e6
+
+-- Fibonacci.hs
+
+f0 :: Int
+f0 = 0
+
+f1 :: Int
+f1 = 1
+
+f2 :: Int
+f2 = f0 + f1
+
+f3 :: Int
+f3 = f1 + f2
+
+f4 :: Int
+f4 = f2 + f3
+
+f5 :: Int
+f5 = f3 + f4
+
+f6 :: Int
+f6 = f4 + f5
+
+f7 :: Int
+f7 = f5 + f6
+
